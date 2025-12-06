2804592089dbcba87972728ddadf2a106f7c6059
Najdz3l <kryspinplplnagler@gmail.com>
2025-12-06 14:57:35 +0100

    Update routes in App component to use SearchResultPage and AddLostPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "src/styles/App.css";
+// Routes
 import Home from "./routes/Home";
+import SearchResultPage from "./routes/SearchResultPage";
+import AddLostPage from "./routes/AddLostPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/search-result" element={<Home />} />
-      <Route path="/add-lost" element={<Home />} />
+      <Route path="/search-result" element={<SearchResultPage />} />
+      <Route path="/add-lost" element={<AddLostPage />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
